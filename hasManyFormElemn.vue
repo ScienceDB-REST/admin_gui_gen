7cdaaa9c8f5ae73349182986fc580ba472183866
vsuaste <suaste@cimat.mx>
2018-11-20 11:57:57 -0600

    scroll list in hasManyForm

--- hasManyFormElemn.vue
+++ hasManyFormElemn.vue
@@ -19,16 +19,20 @@
     </div>
 
     <div class="col-sm-5">
-      <ul v-for="rel in associatedElements" class="list-group">
-        <li v-on:click="removeElement(rel)" class="list-group-item">
-          <i class="delete icon"></i>
-          {{rel[label]}}
-          {{subLabel ? rel[subLabel] : ''}}
-        </li>
-      </ul>
+      <virtual-list class="list"
+          :size="40"
+          :remain="3"
+          :tobottom="toBottom"
+      >
+          <div class="item" v-for="(udf, index) of associatedElements" :key="index">
+              <i v-on:click="removeElement(udf)" class="delete icon"></i>
+            {{ udf[label]}} <span v-if="subLabel!==''"> {{ udf[subLabel]}} </span>
+          </div>
+      </virtual-list>
     </div>
 
   </div>
+
 </template>
 
 <script>
@@ -37,6 +41,7 @@ import Autocomplete from 'vue2-autocomplete-js'
 import axios from 'axios'
 import _ from 'lodash'
 import inflection from 'inflection'
+import virtualList from 'vue-virtual-scroll-list'
 
 Vue.component('autocomplete', Autocomplete)
 
@@ -64,6 +69,7 @@ export default {
   props: ['associatedElements', 'searchUrl', 'label', 'subLabel', 'valueKey','targetModel'],
   components: {
     Autocomplete,
+    'virtual-list' : virtualList
   },
   methods: {
     addElement(data) {
@@ -121,6 +127,9 @@ export default {
       this.query = `query
       ${this.queryName}($search: search${inflection.capitalize(this.targetModel)}Input $pagination: paginationInput)
        {${this.queryName}(search:$search pagination:$pagination){id ${this.label} ${this.parseSublabel}} }`
+    },
+    toBottom(){
+      console.log("To be implemented...??");
     }
   },
   mounted: function(){
@@ -137,4 +146,11 @@ export default {
 </script>
 <style>
 .autocomplete,.autocomplete ul,.autocomplete ul li a,.showAll-transition,.transition{transition:all .3s ease-out;-moz-transition:all .3s ease-out;-webkit-transition:all .3s ease-out;-o-transition:all .3s ease-out}.autocomplete ul{font-family:sans-serif;position:absolute;list-style:none;background:#f8f8f8;padding:10px 0;margin:0;display:inline-block;min-width:15%;margin-top:10px}.autocomplete ul:before{content:"";display:block;position:absolute;height:0;width:0;border:10px solid transparent;border-bottom:10px solid #f8f8f8;left:46%;top:-20px}.autocomplete ul li a{text-decoration:none;display:block;background:#f8f8f8;color:#2b2b2b;padding:5px;padding-left:10px}.autocomplete ul li.focus-list a,.autocomplete ul li a:hover{color:#fff;background:#2f9af7}.autocomplete ul li a .autocomplete-anchor-label,.autocomplete ul li a span{display:block;margin-top:3px;color:grey;font-size:13px}.autocomplete ul li.focus-list a span,.autocomplete ul li a:hover .autocomplete-anchor-label{color:#fff}
+.item {
+    height: 40px;
+    line-height: 50px;
+    padding-left: 20px;
+    border-bottom: 1px solid #eee;
+    background: white;
+}
 </style>
